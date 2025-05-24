c5f6bd86fa187d82c43924f07642af58d50d162e
Ziad Aboalmagd <92919158+ziadwael1812@users.noreply.github.com>
2025-05-24 19:12:50 -0400

    Add MainActivity with Jetpack Compose setup

---
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.fitnesstracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.fitnesstracker.ui.theme.FitnessTrackerTheme // Will be created later
+import com.example.fitnesstracker.ui.navigation.AppNavigation // Will be created later
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FitnessTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
